706fdbacf2eab58698cdd5d3c9e6c58f2b8598ba
funnyfox <foxfunny12@gmail.com>
2021-07-02 16:27:52 +0300

    smth

---
+++ game.h
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+//отрисовка карты
+// Х и О
+//смена хода
+
+class Game{
+    bool game;
+    int player;
+    char X = 'X', O = 'O';
+    std::vector<std::vector<std::string>> table{{"       |       |       \n"},
+        {"   ", "1", "   |   ", "2", "   |   ", "3", "   \n"},
+        {"       |       |       \n"},
+        {"-----------------------\n"},
+        {"       |       |       \n"},
+        {"   ", "4", "   |   ", "5", "   |   ", "6", "   \n"},
+        {"       |       |       \n"},
+        {"-----------------------\n"},
+        {"       |       |       \n"},
+        {"   ", "7", "   |   ", "8", "   |   ", "9", "   \n"},
+        {"       |       |       \n"}
+        };
+    
+    void PrintTable(){
+        for(int i = 0; i < table.size(); i++){
+            for(std::vector<std::string>::iterator it = table[i].begin(); it != table[i].end(); it++){
+                std::cout << (*it);
+            }
+        }
+    }
+
+    public:
+    Game(){
+        PrintTable();
+    }
+    
+};
